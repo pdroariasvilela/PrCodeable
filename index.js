7bc68b5c3571377ba0386ce6e1d9b79339d696ce
pdroariasvilela <pdroariasvilela@gmail.com>
2023-01-20 14:26:43 -0500

    actualizado1

--- index.js
+++ index.js
@@ -4,25 +4,23 @@ let ListaNotas = [
 {nota:'Un arreglo es un conjunto de datos ordenados'},
 ]
 
+
 function cantidadNotas(){
-  console.log(`Usted tiene ${ListaNotas.length} notas`)
     for( let i= 0 ; i < ListaNotas.length ; i++){ 
-    element = ListaNotas[i]
+    let element = ListaNotas[i]
     console.log(`${i}.- ${element.nota}`)
   }
 }
 
-console.log(cantidadNotas())
-console.log('------------------------');
-console.log(`
-1. Crear nota
-2. Editar nota
-3. Eliminar nota`)
-
-
-let eleccion = prompt('Elija una de las opciones')
-
-console.clear()
+function Listado(){
+  console.clear()
+  console.log(`Usted tiene ${ListaNotas.length} notas`)
+  console.log(cantidadNotas());
+  console.log(`------------------------
+  1. Crear nota
+  2. Editar nota
+  3. Eliminar nota`);
+}
 
 // FUNCIONES
 
@@ -42,32 +40,32 @@ function eliminarNota(i) {
   ListaNotas.splice(i, 1);
 }
 
-
-
-
 //---Elecciones
 
+while(true){
 
-if(eleccion == 1){
-  add = prompt('Escriba')
-  añadirNota(add)
-  console.log(cantidadNotas())
-}
+  Listado()
+  let eleccion = prompt('Elige una opción')
+  if(eleccion == 1){
+    add = prompt('Escriba')
+    añadirNota(add)
 
+  } else if(eleccion == 2){
+    let numberEdit = prompt('Que nota desea editar?')
+    let edit = prompt('Escribe la nueva nota')
+    editarNota(numberEdit, edit)
 
-if(eleccion == 2){
-  let numberEdit = prompt('Que nota desea editar?')
-  let edit = prompt('Escribe la nueva nota')
-  editarNota(numberEdit, edit)
-  console.log(cantidadNotas())
-} 
+  } else if(eleccion == 3){
+     i = prompt('Que nota desea eliminar')
+     eliminarNota(i)
+     console.log(cantidadNotas())
 
-if(eleccion == 3){
-   i = prompt('Que nota desea eliminar')
-   eliminarNota(i)
-   console.log(cantidadNotas())
+  }else if (eleccion === null) {
+    console.log("Gracias por usar Notas");
+    break;
 
-}
+}}
+
 
 //actualizar
 
